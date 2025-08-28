d57928e27fa60ea565b9905774f1b20c5d8a4fa4
Artur <noble6107@gmail.com>
2025-08-28 12:45:24 +0200

    second

--- nmcli-connect-eduroam.sh
+++ nmcli-connect-eduroam.sh
@@ -6,6 +6,6 @@ nmcli con add \
   ipv4.method auto \
   802-1x.eap peap \
   802-1x.phase2-auth mschapv2 \
-  802-1x.identity 563064@student.saxion.nl \
-  802-1x.password Qwerty1! \
+  802-1x.identity {username} \
+  802-1x.password {password} \
   wifi-sec.key-mgmt wpa-eap
